5a5be1c775349068f4685fdcac5e191e27611ccd
Charlie Yang <charlesy@stanford.edu>
2014-03-12 15:05:37 -0700

    init load

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -13,6 +13,12 @@ var $gifs = $('img');
 
 function initializePage() {
 	// add any functionality and listeners you want here
+    window.setTimeout(function(){
+        for (var i = 5; i >= 0; i--) {
+            var $new_gif = $($gifs.get([Math.floor(Math.random()*$gifs.length)])).clone();
+            $('.container').append($new_gif);
+        };
+    }, 1000);
     $(window).scroll(function() {
     if($(window).scrollTop() + $(window).height() == $(document).height()) {
         console.log("bottom");
