606bec95e0a75428a4159447f3b4ddb86dcbd882
Charlie Yang <charlesy@stanford.edu>
2014-03-12 14:59:57 -0700

    birthday page for sister

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -8,6 +8,19 @@ $(document).ready(function() {
 /*
  * Function that is called when the document is ready.
  */
+
+var $gifs = $('img');
+
 function initializePage() {
 	// add any functionality and listeners you want here
+    $(window).scroll(function() {
+    if($(window).scrollTop() + $(window).height() == $(document).height()) {
+        console.log("bottom");
+        for (var i = 5; i >= 0; i--) {
+            var $new_gif = $($gifs.get([Math.floor(Math.random()*$gifs.length)])).clone();
+            $('.container').append($new_gif);
+        };
+       }
+    });
 }
+
